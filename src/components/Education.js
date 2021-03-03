a2a6d4e6a2a558df92751ff71d6f64b01fbfe92a
Diesel01 <matheusdiesel01@gmail.com>
2021-03-03 16:04:25 -0300

    Removed the div in Education.js module

--- src/components/Education.js
+++ src/components/Education.js
@@ -4,9 +4,7 @@ export default class Education extends React.Component{
     render(){ 
         const { level, course, institution, startDate, endDate } = this.props; 
         return(
-            <div>
-                <li> {level} in {course} at {institution}, from {startDate} until {endDate} </li>
-            </div>
+            <li> {level} in {course} at {institution}, from {startDate} until {endDate} </li>
         )
         
     }
